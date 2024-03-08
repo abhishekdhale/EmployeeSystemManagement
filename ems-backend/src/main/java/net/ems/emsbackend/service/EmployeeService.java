d748b1c46cc2b01b8e5a920c2219d4296ebd7257
abhishekdhale <abhishekdhale6@gmail.com>
2024-03-08 13:22:59 -0800

    employee service implementation

---
+++ ems-backend/src/main/java/net/ems/emsbackend/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package net.ems.emsbackend.service;
+
+import net.ems.emsbackend.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
---
+++ ems-backend/src/main/java/net/ems/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,28 @@
+package net.ems.emsbackend.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.ems.emsbackend.dto.EmployeeDto;
+import net.ems.emsbackend.entity.Employee;
+import net.ems.emsbackend.mapper.EmployeeMapper;
+import net.ems.emsbackend.repository.EmployeeRepository;
+import net.ems.emsbackend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee= EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEMployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEMployee);
+    }
+}
