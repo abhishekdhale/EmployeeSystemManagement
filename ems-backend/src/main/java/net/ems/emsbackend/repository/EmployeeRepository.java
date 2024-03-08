45016d93287585134d1d85d76608144f1bbdb670
abhishekdhale <abhishekdhale6@gmail.com>
2024-03-08 12:19:54 -0800

    Employee class and JPA repository

---
+++ ems-backend/src/main/java/net/ems/emsbackend/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package net.ems.emsbackend.repository;
+
+import net.ems.emsbackend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
